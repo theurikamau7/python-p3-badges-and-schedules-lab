42cd8452daf82286680517fd8fc404f32632492f
IanKamau <theurikamau7@gmail.com>
2023-10-09 01:07:46 +0300

    first commit

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,8 +1,11 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
     return None
